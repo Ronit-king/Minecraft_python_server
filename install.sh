08c6b45508424834136971ad6194e002a2ac9146
Ronit Mishra <ronitmishra@Ronits-MacBook-Pro.local>
2025-09-15 18:54:26 -0500

    Wrapper file to run the installer and restart the shell

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run your Python installer script
+python3 Azul_installer.py
+
+# Reloads the shell environment so JAVA_HOME/PATH takes effect immediately
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+elif [ -f ~/.profile ]; then
+    source ~/.profile
+fi
+
+# Verify Installation
+
+echo
+echo "✅ Java is ready to use. Checking Java version..."
+java -version
